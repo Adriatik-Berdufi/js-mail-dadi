53497d0c7b6c9ae8bb12db0adaf7bd5e192b9cf7
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-12 18:03:03 +0100

    feat: add button per giocare

--- dadi/main.js
+++ dadi/main.js
@@ -1,29 +1,39 @@
 const diceFace = [1,2,3,4,5,6];
 let userScore = 0;
 let computerScore = 0;
+const play = document.getElementById("play");
 // random 2 numeri 
-
+//Play
 
 // impostiamo un ciclo per 9 partite 
-for(i = 10; i > 0;i--){
-    let user = diceFace[Math.floor(Math.random() * 6)];
-    console.log(user);
-    console.log("VS");
-    let computer = diceFace[Math.floor(Math.random() * 6)];
-    console.log(computer);
-    // troviamo il piu grande 
-    // assegnamo 1 punto al vincitore
-    if(user > computer){
-        console.log("You win");
-        userScore ++;
-    }else if(user < computer){
-        console.log("You Lose");
-        computerScore ++;
-    }else{
-        console.log("Drow");
+let partita = 0;
+play.addEventListener('click' ,function(){
+
+    if(partita < 10){
+        let user = diceFace[Math.floor(Math.random() * 6)];
+        console.log(user);
+        console.log("VS");
+        let computer = diceFace[Math.floor(Math.random() * 6)];
+        console.log(computer);
+        // troviamo il piu grande 
+        // assegnamo 1 punto al vincitore
+        if(user > computer){
+            console.log("You win");
+            userScore ++;
+            console.log(userScore);
+        }else if(user < computer){
+            console.log("You Lose");
+            computerScore ++;
+        }else{
+            console.log("Drow");
+        }
+        console.log("RISULTATO FINALE")
+        console.log(userScore);
+        console.log(computerScore);
+        partita ++;
     }
-   
-}
-console.log("RISULTATO FINALE")
-console.log(userScore);
-console.log(computerScore);
+
+        
+});
+
+
